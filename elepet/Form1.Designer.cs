b31833d37a528bb4154b92bb176938f7b9282c1d
s1120372@o365st.pu.edu.tw <s1120372@o365st.pu.edu.tw>
2024-06-01 16:28:10 +0800

    加入專案檔案。

---
+++ elepet/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace elepet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("正謙櫻桃小丸子字", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(673, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 126);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "餵食";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("雙魚集 夏生毛筆字", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 53);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 592);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("雙魚集 夏生毛筆字", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "我的電子寵物垃圾桶";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ elepet/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace elepet
+{
+    public partial class Form1 : Form
+    {
+        private int hunger = 100;
+        private Label lblHunger;
+        private Button btnFeed;
+        private Timer timerUpdate;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+        private void InitializeControls()
+        {
+            // 初始化控制項
+            timerUpdate = new Timer();
+
+            // 設定控制項屬性
+            label1.Text = "飢餓：100";
+            timerUpdate.Interval = 1000; // 1秒
+
+
+            // 添加事件處理器
+            timerUpdate.Tick += timerUpdate_Tick;
+
+            // 啟動計時器
+            timerUpdate.Start();
+        }
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            // 每秒更新寵物狀態
+            hunger -= 1;
+            UpdatePetStatus();
+        }
+        private void UpdatePetStatus()
+        {
+            label1.Text = $"飢餓：{hunger}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 寵物被餵食
+            hunger += 10;
+            UpdatePetStatus();
+        }
+    }
+}
